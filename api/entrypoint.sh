55e1c013c7e0c7e86394905dfba75cd0825364e1
Rowland Henshaw <rhenshaw@tactable.io>
2023-02-13 08:53:57 +0100

    saving changes

---
+++ api/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python3 manage.py create_db
+
+exec python3 -m flask run --host=0.0.0.0
